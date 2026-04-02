4423c485bbceb715d92ca78965ea7b3ed3e99a69
unknown <byrisrinidhi@gmail.com>
2026-04-03 04:07:04 +0530

    Added task structure with deadlines and improved CLI interface

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ tasks = []
 memoryNotes = {}
 
 def addTask():
-    task = input("Add a task: ").strip()
-    if task == "":
+    name = input("Enter task name: ").strip()
+    deadline = input("Enter deadline (YYYY-MM-DD): ").strip()
+
+    if name == "":
         print("Task cannot be empty!")
         return
+
+    task = {"Name": name, "deadline": deadline}
     tasks.append(task)
+
     print("Task added successfully!")
 
 def memoryNote():
@@ -21,34 +26,36 @@ def viewTasks():
     else:
         print("\nYour Tasks:")
         for i in range(len(tasks)):
-            print(i+1, "-", tasks[i])
+            print(i+1, "-", tasks[i]["Name"], "| Deadline:", tasks[i]["deadline"])
+        print()
 
 def searchMemory():
-    item = input("Enter item to search: ").lower()
-    if item in memoryNotes:
-        print(f"{item} is in {memoryNotes[item]}")
-    else:
-        print("Item not found")
+    item = input("Enter item to search: ").lower().strip()
+    print("Result:", memoryNotes.get(item, "Not found"))
 
 while True:
     print("\n==============================")
     print("Welcome to MINDLINK CLI")
     print("==============================")
 
-    print("1. Add Task")
+    print("\n1. Add Task")
     print("2. Save Memory Note")
     print("3. View Tasks")
     print("4. Search Memory")
     print("5. Exit")
 
-    choice = input("Enter choice: ")
+    choice = input("\nEnter choice: ")
 
-    if choice == "1": addTask()
-    elif choice == "2": memoryNote()
-    elif choice == "3": viewTasks()
-    elif choice == "4": searchMemory()
+    if choice == "1":
+        addTask()
+    elif choice == "2":
+        memoryNote()
+    elif choice == "3":
+        viewTasks()
+    elif choice == "4":
+        searchMemory()
     elif choice == "5":
-        print("Thank you : )")
+        print("\nThank you for using Mindlink!")
         break
     else:
-        print("Invalid choice")
+        print("Invalid choice, try again")
