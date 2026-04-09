8985245580ef1d24e9e5afca7c1df61071a62415
unknown <byrisrinidhi@gmail.com>
2026-04-09 19:28:00 +0530

    Improved file handling, added persistent storage, and made data loading safer

--- main.py
+++ main.py
@@ -5,26 +5,75 @@ memoryNotes = {}
 todayPlan = []
 
 
+def loadTasks():
+    try:
+        with open("tasks.txt", "r") as file:
+            for line in file:
+                parts = line.strip().split("|")
+                if len(parts) == 3:
+                    name, deadline, priority = parts
+                    tasks.append({"Name": name, "deadline": deadline, "priority": priority})
+    except:
+        pass
+
+def loadMemory():
+    try:
+        with open("memory.txt", "r") as file:
+            for line in file:
+                parts = line.strip().split("|")
+                if len(parts) == 2:
+                    item, loc = parts
+                    memoryNotes[item] = loc
+    except:
+        pass
+
+def loadPlan():
+    try:
+        with open("today.txt", "r") as file:
+            for line in file:
+                todayPlan.append(line.strip())
+    except:
+        pass
+
+
+def saveTasks():
+    with open("tasks.txt", "w") as file:
+        for task in tasks:
+            file.write(task["Name"] + "|" + task["deadline"] + "|" + task["priority"] + "\n")
+
+def saveMemory():
+    with open("memory.txt", "w") as file:
+        for item in memoryNotes:
+            file.write(item + "|" + memoryNotes[item] + "\n")
+
+def savePlan():
+    with open("today.txt", "w") as file:
+        for plan in todayPlan:
+            file.write(plan + "\n")
+
+
 def addTask():
-    print("\n Add New Task")
     name = input("Enter task name: ").strip()
     deadline = input("Enter deadline (YYYY-MM-DD): ").strip()
+    priority = input("Enter priority (High/Medium/Low): ").strip()
 
     if name == "":
         print("Task cannot be empty!")
         return
 
-    tasks.append({"Name": name, "deadline": deadline})
-    print("Task added!")
+    tasks.append({"Name": name, "deadline": deadline, "priority": priority})
+    saveTasks()
+
+    print("Task added successfully!")
 
 def viewTasks():
     if len(tasks) == 0:
-        print("\nNo tasks available")
+        print("No tasks available")
         return
 
-    print("\nYour Tasks:")
+    print("\nTasks:")
     for i in range(len(tasks)):
-        print(f"{i+1}. {tasks[i]['Name']} | Deadline: {tasks[i]['deadline']}")
+        print(i+1, "-", tasks[i]["Name"], "| Deadline:", tasks[i]["deadline"], "| Priority:", tasks[i]["priority"])
 
 def deleteTask():
     viewTasks()
@@ -32,60 +81,56 @@ def deleteTask():
         return
 
     try:
-        num = int(input("\nEnter task number: "))
+        num = int(input("Enter task number: "))
         if 1 <= num <= len(tasks):
-            removed = tasks.pop(num-1)
-            print(f" Deleted: {removed['Name']}")
-        else:
-            print("Invalid number")
+            tasks.pop(num-1)
+            saveTasks()
+            print("Task deleted!")
     except:
-        print("Enter valid number")
+        print("Invalid input")
 
 
 def memoryNote():
-    print("\nSave Memory")
     item = input("Item: ").lower().strip()
     loc = input("Location: ").strip()
 
-    if item == "" or loc == "":
-        print("Cannot be empty")
-        return
-
     memoryNotes[item] = loc
-    print("Saved!")
+    saveMemory()
+
+    print("Memory saved!")
 
 def viewMemoryNotes():
     if len(memoryNotes) == 0:
-        print("\nNo memory notes")
-    else:
-        print("\nMemory Notes:")
-        for item in memoryNotes:
-            print(f"{item} → {memoryNotes[item]}")
+        print("No memory notes")
+        return
+
+    print("\nMemory Notes:")
+    for item in memoryNotes:
+        print(item, "->", memoryNotes[item])
 
 def searchMemory():
-    item = input("\nSearch item: ").lower().strip()
-    print("Result:", memoryNotes.get(item, "Not found"))
-
+    item = input("Search item: ").lower()
+    print(memoryNotes.get(item, "Not found"))
 
 
 def addTodayPlan():
-    print("\nAdd Today's Plan")
-    plan = input("Enter plan: ").strip()
-
+    plan = input("Enter today's plan: ").strip()
     if plan == "":
-        print("Plan cannot be empty!")
+        print("Empty plan!")
         return
 
     todayPlan.append(plan)
+    savePlan()
     print("Plan added!")
 
 def viewTodayPlan():
     if len(todayPlan) == 0:
-        print("\nNo plans for today")
-    else:
-        print("\nToday's Plans:")
-        for i in range(len(todayPlan)):
-            print(f"{i+1}. {todayPlan[i]}")
+        print("No plans today")
+        return
+
+    print("\nToday Plans:")
+    for i in range(len(todayPlan)):
+        print(i+1, "-", todayPlan[i])
 
 def deletePlan():
     viewTodayPlan()
@@ -93,47 +138,50 @@ def deletePlan():
         return
 
     try:
-        num = int(input("\nEnter plan number: "))
+        num = int(input("Enter number: "))
         if 1 <= num <= len(todayPlan):
-            removed = todayPlan.pop(num-1)
-            print(f"Removed: {removed}")
-        else:
-            print("Invalid number")
+            todayPlan.pop(num-1)
+            savePlan()
+            print("Plan removed!")
     except:
-        print("Enter valid number")
+        print("Invalid input")
 
+
+loadTasks()
+loadMemory()
+loadPlan()
+
+print("Data loaded successfully")
 
 while True:
     print("\n==============================")
     print("Welcome to MINDLINK CLI")
-    print("Your External Memory Assistant")
     print("==============================")
 
-    print("\nChoose an option:")
-    print("1. Add Task")
-    print("2. View Tasks")
-    print("3. Delete Task")
-    print("4. Save Memory")
-    print("5. View Memory")
-    print("6. Search Memory")
-    print("7. Add Today Plan")
-    print("8. View Today Plan")
-    print("9. Delete Today Plan")
-    print("10. Exit")
+    print("1 Add Task")
+    print("2 View Tasks")
+    print("3 Delete Task")
+    print("4 Add Memory")
+    print("5 View Memory")
+    print("6 Search Memory")
+    print("7 Add Plan")
+    print("8 View Plan")
+    print("9 Delete Plan")
+    print("10 Exit")
 
-    ch = input("\nEnter your choice: ")
+    choice = input("Enter choice: ")
 
-    if ch == "1": addTask()
-    elif ch == "2": viewTasks()
-    elif ch == "3": deleteTask()
-    elif ch == "4": memoryNote()
-    elif ch == "5": viewMemoryNotes()
-    elif ch == "6": searchMemory()
-    elif ch == "7": addTodayPlan()
-    elif ch == "8": viewTodayPlan()
-    elif ch == "9": deletePlan()
-    elif ch == "10":
-        print("\nThank you for using Mindlink!")
+    if choice == "1": addTask()
+    elif choice == "2": viewTasks()
+    elif choice == "3": deleteTask()
+    elif choice == "4": memoryNote()
+    elif choice == "5": viewMemoryNotes()
+    elif choice == "6": searchMemory()
+    elif choice == "7": addTodayPlan()
+    elif choice == "8": viewTodayPlan()
+    elif choice == "9": deletePlan()
+    elif choice == "10":
+        print("Thank you!")
         break
     else:
-        print("Choose valid option (1-10)")
+        print("Invalid choice")
